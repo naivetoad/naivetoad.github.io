aafeb8252c7fdb72b6d0024a6f1a6c830f48527b
naivetoad <143419238+naivetoad@users.noreply.github.com>
2025-03-09 16:50:58 +0000

    Deploying to gh-pages from @ naivetoad/naivetoad.github.io@f2e96a7b866f1c17051ffc9a652973765b68f41e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -100,6 +100,13 @@ ninja.data = [{
           window.open("https://github.com/naivetoad", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/weiwuyan", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
